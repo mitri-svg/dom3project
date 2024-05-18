a240a3d24136b32f7a3a6177e3e76b8c77092de6
Mitri <hmirseid54@gmail.com>
2024-05-18 13:42:02 +0400

    project yaradi ve qurtardi

---
+++ script.js
@@ -0,0 +1,179 @@
+const cavab1 = document.querySelector('.ronaldo')
+const cavab2 = document.querySelector('.messi')
+const cavab3 = document.querySelector('.neymar')
+const cavab4 = document.querySelector('.rodrygo')
+const cavab5 = document.querySelector('.greyfrut')
+const cavab6 = document.querySelector('.kavun')
+const cavab7 = document.querySelector('.drakonMeyve')
+const cavab8 = document.querySelector('.limon')
+const cavab9 = document.querySelector('.sahdag')
+const cavab10 = document.querySelector('.qapiciq')
+const cavab11 = document.querySelector('.bazarduzu')
+const cavab12 = document.querySelector('.kepez')
+const cavab13 = document.querySelector('.sarisu')
+const cavab14 = document.querySelector('.ag')
+const cavab15 = document.querySelector('.maralgol')
+const cavab16 = document.querySelector('.goygol')
+const cavab17 = document.querySelector('.rusiya')
+const cavab18 = document.querySelector('.almaniya')
+const cavab19 = document.querySelector('.isvecre')
+const cavab20 = document.querySelector('.azerbaycan')
+const cavab21 = document.querySelector('.turkiyeFlag')
+const cavab22 = document.querySelector('.almaniyaFlag')
+const cavab23 = document.querySelector('.qazaxistanFlag')
+const cavab24 = document.querySelector('.polsa')
+const cavab25 = document.querySelector('.neftci')
+const cavab26 = document.querySelector('.qarabag')
+const cavab27 = document.querySelector('.zire')
+const cavab28 = document.querySelector('.sebail')
+
+cavab1.addEventListener('click', () => {
+    cavab1.style.backgroundImage = 'url(ronaldo.jpeg)'
+    cavab1.style.backgroundSize = 'cover'
+    cavab1.innerHTML = '';
+})
+cavab2.addEventListener('click', () => {
+    cavab2.style.backgroundImage = 'url(messi.jpeg)'
+    cavab2.style.backgroundSize = 'cover'
+    cavab2.innerHTML = '';
+})
+cavab3.addEventListener('click', () => {
+    cavab3.style.backgroundImage = 'url(neymar.jpeg)'
+    cavab3.style.backgroundSize = 'cover'
+    cavab3.innerHTML = '';
+})
+cavab4.addEventListener('click', () => {
+    cavab4.style.backgroundImage = 'url(rodrygo.jpeg)'
+    cavab4.style.backgroundSize = 'cover'
+    cavab4.innerHTML = '';
+})
+cavab5.addEventListener('click', () => {
+    cavab5.style.backgroundImage = 'url(greyfrut.jpeg)'
+    cavab5.style.backgroundSize = 'cover'
+    cavab5.innerHTML = '';
+})
+cavab6.addEventListener('click', () => {
+    cavab6.style.backgroundImage = 'url(kavun.jpeg)'
+    cavab6.style.backgroundSize = 'cover'
+    cavab6.style.backgroundPosition = 'center'
+    cavab6.innerHTML = '';
+})
+cavab7.addEventListener('click', () => {
+    cavab7.style.backgroundImage = 'url(drakonMeyve.jpeg)'
+    cavab7.style.backgroundSize = 'cover'
+    cavab7.innerHTML = '';
+})
+cavab8.addEventListener('click', () => {
+    cavab8.style.backgroundImage = 'url(limon.jpeg)'
+    cavab8.style.backgroundSize = 'cover'
+    cavab8.innerHTML = '';
+})
+cavab9.addEventListener('click', () => {
+    cavab9.style.backgroundImage = 'url(sahdag.jpeg)'
+    cavab9.style.backgroundSize = 'cover'
+    cavab9.innerHTML = '';
+})
+cavab10.addEventListener('click', () => {
+    cavab10.style.backgroundImage = 'url(qapiciq.jpeg)'
+    cavab10.style.backgroundSize = 'cover'
+    cavab10.style.backgroundPosition = 'center'
+    cavab10.innerHTML = '';
+})
+cavab11.addEventListener('click', () => {
+    cavab11.style.backgroundImage = 'url(bazarduzu.jpeg)'
+    cavab11.style.backgroundSize = 'cover'
+    cavab11.innerHTML = '';
+})
+cavab12.addEventListener('click', () => {
+    cavab12.style.backgroundImage = 'url(kepez.jpeg)'
+    cavab12.style.backgroundSize = 'cover'
+    cavab12.innerHTML = '';
+})
+cavab13.addEventListener('click', () => {
+    cavab13.style.backgroundImage = 'url(sarisu.jpeg)'
+    cavab13.style.backgroundSize = 'cover'
+    cavab13.innerHTML = '';
+})
+cavab14.addEventListener('click', () => {
+    cavab14.style.backgroundImage = 'url(ag.jpeg)'
+    cavab14.style.backgroundSize = 'cover'
+    cavab14.style.backgroundPosition = 'center'
+    cavab14.innerHTML = '';
+})
+cavab15.addEventListener('click', () => {
+    cavab15.style.backgroundImage = 'url(maralgol.jpeg)'
+    cavab15.style.backgroundSize = 'cover'
+    cavab15.innerHTML = '';
+})
+cavab16.addEventListener('click', () => {
+    cavab16.style.backgroundImage = 'url(goygol.jpeg)'
+    cavab16.style.backgroundSize = 'cover'
+    cavab16.innerHTML = '';
+})
+cavab17.addEventListener('click', () => {
+    cavab17.style.backgroundImage = 'url(rusiya.jpeg)'
+    cavab17.style.backgroundSize = 'cover'
+    cavab17.innerHTML = '';
+})
+cavab18.addEventListener('click', () => {
+    cavab18.style.backgroundImage = 'url(almaniya.jpeg)'
+    cavab18.style.backgroundSize = 'cover'
+    cavab18.style.backgroundPosition = 'center'
+    cavab18.innerHTML = '';
+})
+cavab19.addEventListener('click', () => {
+    cavab19.style.backgroundImage = 'url(isvecre.jpeg)'
+    cavab19.style.backgroundSize = 'cover'
+    cavab19.innerHTML = '';
+})
+cavab20.addEventListener('click', () => {
+    cavab20.style.backgroundImage = 'url(azerbaycan.jpeg)'
+    cavab20.style.backgroundSize = 'cover'
+    cavab20.innerHTML = '';
+})
+cavab21.addEventListener('click', () => {
+    cavab21.style.backgroundImage = 'url(turkiyeFlag.jpeg)'
+    cavab21.style.backgroundSize = 'cover'
+    cavab21.innerHTML = '';
+})
+cavab22.addEventListener('click', () => {
+    cavab22.style.backgroundImage = 'url(almaniyaFlag.jpeg)'
+    cavab22.style.backgroundSize = 'cover'
+    cavab22.style.backgroundPosition = 'center'
+    cavab22.innerHTML = '';
+})
+cavab23.addEventListener('click', () => {
+    cavab23.style.backgroundImage = 'url(qazaxistanFlag.jpeg)'
+    cavab23.style.backgroundSize = 'cover'
+    cavab23.innerHTML = '';
+})
+cavab24.addEventListener('click', () => {
+    cavab24.style.backgroundImage = 'url(polsa.jpeg)'
+    cavab24.style.backgroundSize = 'cover'
+    cavab24.style.backgroundPosition = 'center'
+    cavab24.innerHTML = '';
+})
+cavab25.addEventListener('click', () => {
+    cavab25.style.backgroundImage = 'url(neftci.png)'
+    cavab25.style.backgroundSize = 'cover'
+    cavab25.style.backgroundPosition = 'center'
+    cavab25.innerHTML = '';
+})
+cavab26.addEventListener('click', () => {
+    cavab26.style.backgroundImage = 'url(qarabag.png)'
+    cavab26.style.backgroundSize = 'cover'
+    cavab26.style.backgroundPosition = 'center'
+    cavab26.innerHTML = '';
+})
+cavab27.addEventListener('click', () => {
+    cavab27.style.backgroundImage = 'url(zire.jpeg)'
+    cavab27.style.backgroundSize = 'cover'
+    cavab27.style.backgroundPosition = 'center'
+    cavab27.innerHTML = '';
+})
+cavab28.addEventListener('click', () => {
+    cavab28.style.backgroundImage = 'url(sebail.png)'
+    cavab28.style.backgroundSize = 'cover'
+    cavab28.style.backgroundPosition = 'center'
+    cavab28.innerHTML = '';
+})
